9a6f40777da294a820a9ba71f7c1daa717859369
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 00:00:25 +0530

    Daily Employee wage

--- EmpTest.java
+++ EmpTest.java
@@ -4,6 +4,8 @@ public class EmpTest {
 	public static void main(String[] args) {
 		EmployeeWage emp=new EmployeeWage();
 		emp.Presenty();
+		int EmpWage=emp.DailyEmployeeWage();
+		System.out.println("Daily Employee wage is: "+EmpWage);
 
 	}
 
