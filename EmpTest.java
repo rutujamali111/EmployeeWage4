d3a6243aa304caebc12014483173c85b42a56a17
rutuja_mali <rutujamali111@gmail.com>
2022-07-27 23:52:19 +0530

    presenty

---
+++ EmpTest.java
@@ -0,0 +1,10 @@
+
+public class EmpTest {
+
+	public static void main(String[] args) {
+		EmployeeWage emp=new EmployeeWage();
+		emp.Presenty();
+
+	}
+
+}
---
+++ EmployeeWage.java
@@ -0,0 +1,12 @@
+
+public class EmployeeWage {
+	public void Presenty()
+	{
+		int IS_FULL_TIME=1;
+		double empCheck=(Math.random()*10)%2;
+		if(empCheck==IS_FULL_TIME)
+			System.out.println("Employee is present..");
+		else
+			System.out.println("Employee is Absent");
+	}
+}
